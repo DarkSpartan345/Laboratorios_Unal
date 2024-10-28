7243d496876acfea822fbce3cfc9f9db65a659b1
DarkSpartan345 <raruedam@udistrital.edu.co>
2024-10-28 10:46:20 -0500

    prueba de entrada

--- Labs_Unal/Labs_Unal/settings.py
+++ Labs_Unal/Labs_Unal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6ob8mu^%u5&p2)h094g&8zg3su5p+zb(#85vt%s^=-t$7k2ra1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
