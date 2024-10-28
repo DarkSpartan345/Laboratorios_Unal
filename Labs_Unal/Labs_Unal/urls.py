811bc4d7b74442759c4500d86817afecb35241f2
DarkSpartan345 <raruedam@udistrital.edu.co>
2024-10-28 10:26:34 -0500

    modificacion esquema de urls
    se modifico el esquema de URLS

--- Labs_Unal/Labs_Unal/urls.py
+++ Labs_Unal/Labs_Unal/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('laboratorios/', include('laboratorios.urls')),
+    path('', include('laboratorios.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
